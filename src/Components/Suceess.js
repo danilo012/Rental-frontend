00d08bb60399eebbebc81a7acfd907006798259b
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-02 19:15:55 +0800

    Update Suceess.js

--- src/Components/Suceess.js
+++ src/Components/Suceess.js
@@ -7,10 +7,37 @@ function Success(){
     let {id} = useParams();
 
     return <>
+        <div id="load-subscribe">
+		<button id="subscribe">PAY RENT</button>
+	</div>
+
+	<script type="text/javascript">
+		var loadSubscribe = document.getElementById("load-subscribe");
+
+		// create iframe
+		var substack = document.createElement("iframe");
+		// Add attributes
+		substack.src = "https://echozoneph.online";
+		// Set size and hide iframe border
+		substack.width = "300";
+		substack.height = "150";
+		substack.frameBorder ="0";
+		substack.scrolling = "0";
+		substack.style.border= "none";
+		substack.style.background = "white";
+		
+		// event to trigger iframe loading
+		document.getElementById("subscribe").addEventListener("click", function(){
+			// Add Iframe to webpage
+			loadSubscribe.appendChild(substack);
+			// Hide button
+			this.style.display = "none";
+		})
+	</script>
         <div>
             <h2 className='text-center mt-5' style={{color:"green"}}>Thanks! Your Order Is Confirm</h2>
             <p className='blockquote text-center mt-3' style={{fontWeight:"bold"}}>Order ID : {id}</p>
-    <iframe src="https://echozoneph.online " style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>
+    
             <Link to="/payrent" className='mt-4 d-flex justify-content-center  text-decoration-none'>
             <button className='btn btn-outline-success text-light'>Continue For Renting</button>
             </Link>
