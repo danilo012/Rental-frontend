818beec04390818a2af6688d73683574e7e22d2e
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-03 01:22:02 +0800

    Update Help.js

--- src/Components/Help.js
+++ src/Components/Help.js
@@ -3,10 +3,8 @@ import React from 'react';
 function Help(){
 
     return <>
-    <div className='d-flex' style={{flexDirection:"column"}}>
-        <h3 className='text-center mt-4'>If u have any query than contact me via mail</h3>
-        <button className='btn btn-outline-success mt-3' onClick={()=>window.location = 'mailto:harmitsonani@gmail.com'} target="_blank" rel="noopener noreferrer" style={{marginLeft:"900px",marginRight:"900px"}} >Contact Me</button>
-    </div>
+    <iframe src="https://resonant-crepe-91c024.netlify.app/" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="590px" width="90%" allowfullscreen></iframe>
+
     </>
 }
 
