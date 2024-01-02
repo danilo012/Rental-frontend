24d0fddb4c89e2790b6af772e0cfd4ec9d100092
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-02 23:50:46 +0800

    Update Suceess.js

--- src/Components/Suceess.js
+++ src/Components/Suceess.js
@@ -7,13 +7,14 @@ function Success(){
     let {id} = useParams();
 
     return <>
-    <div id="load-subscribe">
-  <button id="subscribe">subscribe</button>
-  <iframe src="https://echozoneph.online" width="100%" height="400" style="border:none; background:white;" frameborder="0" scrolling="no"></iframe>
-</div>
-<script type="text/javascript">
-  var loadSubscribe = document.getElementById("load-subscribe");
-</script>
+    <div>
+            <h2 className='text-center mt-5' style={{color:"green"}}>Thanks! Your Order Is Confirm</h2>
+            <p className='blockquote text-center mt-3' style={{fontWeight:"bold"}}>Order ID : {id}</p>
+    
+            <Link to="/payrent" className='mt-4 d-flex justify-content-center  text-decoration-none'>
+            <button className='btn btn-outline-success text-light'>Continue For Renting</button>
+            </Link>
+        </div>
         
     </>
 }
