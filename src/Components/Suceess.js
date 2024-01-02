8a1a875a50fc1908b59f67fc267d2daab876b547
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-02 18:38:51 +0800

    Update Suceess.js

--- src/Components/Suceess.js
+++ src/Components/Suceess.js
@@ -10,7 +10,7 @@ function Success(){
         <div>
             <h2 className='text-center mt-5' style={{color:"green"}}>Thanks! Your Order Is Confirm</h2>
             <p className='blockquote text-center mt-3' style={{fontWeight:"bold"}}>Order ID : {id}</p>
-            <Link to="/payrent.html" className='mt-4 d-flex justify-content-center  text-decoration-none'>
+            <Link to="/payrent" className='mt-4 d-flex justify-content-center  text-decoration-none'>
             <button className='btn btn-outline-success text-light'>Continue For Renting</button>
             </Link>
         </div>
