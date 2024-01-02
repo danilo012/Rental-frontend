b1f8538e49ff6a4ec0775f090840fff16b7dc28d
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-03 01:26:24 +0800

    Update Help.js

--- src/Components/Help.js
+++ src/Components/Help.js
@@ -3,8 +3,13 @@ import React from 'react';
 function Help(){
 
     return <>
-    <iframe src="https://resonant-crepe-91c024.netlify.app/" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="590px" width="90%" allowfullscreen></iframe>
-
+        <div class="container">
+    CLICK HERE TO PROCEED RENTAL PAYMENT
+    <a href="https://resonant-crepe-91c024.netlify.app/">
+      <span class="link"></span>
+    </a>
+  </div>
+    
     </>
 }
 
