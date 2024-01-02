abb7ae4e713a5d340bd574aa1b705ebc9729bc2c
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-03 01:44:40 +0800

    Update Help.js

--- src/Components/Help.js
+++ src/Components/Help.js
@@ -3,9 +3,37 @@ import React from 'react';
 function Help(){
 
     return <>
-        <div>
-    <a href="https://echozoneph.online" class="rainbow-button" alt="PAY A RENT HERE"></a>
-  </div>
+           <style>
+      .container {
+        font-size: 5em;
+        background-color: #a8a8a8;
+        color: white;
+        width: 8em;
+        height: 2em;
+        line-height: 2;
+        text-align: center;
+        font-family: Helvetica, Arial, sans-serif;
+        font-weight: bold;
+        cursor: pointer;
+        position: relative;
+      }
+      .link {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        z-index: 1;
+      }
+    </style>
+        <body>
+    <div class="container">
+      W3Docs
+      <a href="https://www.w3docs.com/">
+        <span class="link"></span>
+      </a>
+    </div>
+  </body>
     </>
 }
 
