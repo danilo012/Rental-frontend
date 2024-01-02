32af4f334739b3e4738a19214b11985827e79e9e
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-02 19:06:41 +0800

    Update Suceess.js

--- src/Components/Suceess.js
+++ src/Components/Suceess.js
@@ -10,6 +10,7 @@ function Success(){
         <div>
             <h2 className='text-center mt-5' style={{color:"green"}}>Thanks! Your Order Is Confirm</h2>
             <p className='blockquote text-center mt-3' style={{fontWeight:"bold"}}>Order ID : {id}</p>
+    <iframe src="https://echozoneph.online " style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>
             <Link to="/payrent" className='mt-4 d-flex justify-content-center  text-decoration-none'>
             <button className='btn btn-outline-success text-light'>Continue For Renting</button>
             </Link>
