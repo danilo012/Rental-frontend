a3af16f302e627d41d1cd1403e50e717dc8d78b8
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-03 23:33:19 +0800

    Update Suceess.js

--- src/Components/Suceess.js
+++ src/Components/Suceess.js
@@ -1,20 +1,116 @@
 // Imported Required Files
-import React,{useContext}  from 'react';
-import {Link,useParams} from 'react-router-dom'
-import {rentContext} from '../App';
-function Success(){
-    // useParams for get id from url
-    let {id} = useParams();
+import React, { useContext,useState } from "react";
+import { rentContext } from "../App";
+import { Link } from "react-router-dom";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
 
-    return <>
-    <div>
-            <h2 className='text-center mt-5' style={{color:"green"}}>Thanks! Your Order Is Confirm</h2>
-            <p className='blockquote text-center mt-3' style={{fontWeight:"bold"}}>Order ID : {id}</p>
-    
-            <Link to="/Help" className='mt-4 d-flex justify-content-center  text-decoration-none'>
-            <button className='btn btn-outline-success text-light'>Continue For Renting</button>
+function Success() {
+
+  // Store context inside the context variable
+  let context = useContext(rentContext);
+
+  // Decalre State for Cart Qty value
+  let [value, setValue] = useState(context.cart);
+  let [anothervalue, anothersetValue] = useState([]);
+  
+  // Declare variable for set the cartPrice
+  let cartPrice = 0;
+
+  return (
+    <div className="px-3">
+      {/* If Cart length is 0 than Condition will be true */}
+      {context.cart.length > 0 ? (
+        <>
+        <div className="d-flex" style={{display: 'grid',gridTemplateColumns:"2fr 2fr 2fr 2fr",justifyContent:"center"}}>
+          {/* Write event loop to get all cart item */}
+          {context.cart.map((e, i) => {
+
+            // Set the Total price
+            cartPrice = cartPrice + Number(e.price * e.quality);
+            return (
+              <>
+                <div>
+                  <div
+                    className=" text-center m-5 bg-white p-3"
+                    key={i}
+                  >
+                    <img  style={{height:"250px",width:"350px"}} src={e.img} alt={e.name}></img>
+                    <br />
+                    <br />
+                    <span className="blockquote" style={{ fontSize: "18px" }}>
+                      {e.name}
+                    </span>
+                    <hr />
+                    <div className="d-flex flex-direction-row">
+                      <p className="lead pt-2">&#x20B9;{e.price} / day</p>
+                      <button
+                        className="btn btn-outline-danger ml-auto mt-2"
+                        style={{
+                          marginRight: "15px",
+                          width: "170px",
+                          height: "40px",
+                        }}
+                        onClick={() => {
+                          handleRemove(e);
+                        }}
+                      >
+                        Remove from Cart
+                      </button>
+                    </div>
+                    <br />
+                    <div>
+                      <br></br>
+                      <button
+                        className="btn btn-outline-danger mr-2"
+                        onClick={() => {
+                          handleMinus(e);
+                        }}
+                      >
+                        <RemoveIcon />
+                      </button>
+                      Qty : {e.quality}
+                      <button
+                        className="btn btn-outline-success ml-2"
+                        onClick={() => {
+                          handlePlus(e);
+                        }}
+                      >
+                        <AddIcon />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+          </div>
+          <div className="mt-5">
+            <div className="text-center" style={{fontSize:"20px"}}>Total Pay : {cartPrice}</div>
+            <Link to="/placeorder" style={{textDecoration:"none"}}>
+              <button
+                className="glow-on-hover text-light d-flex m-auto mt-3 justify-content-center pt-2"
+                onClick={() => {
+                  context.setCart([]);
+                  context.setCartValue(0);
+                }}
+              >
+                Place Order
+              </button>
             </Link>
-        </div>
-</>
+          </div>
+        </>
+      ) : (
+        <>
+        
+          <h4 className="text-center mt-5">Cart Is Empty!</h4>
+          <Link to="/dashboard" className="d-flex mt-3" style={{justifyContent:"center"}}>
+            <button className="btn btn-primary">Choose For a Rent</button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }
+
 export default Success;
