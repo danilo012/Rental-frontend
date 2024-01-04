23083bf7d0f97f691684579bfbf3bd6e6b020e6c
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-04 18:38:58 +0800

    Update Camera.js

--- src/Pages/Camera.js
+++ src/Pages/Camera.js
@@ -2,47 +2,12 @@ import React, { useContext } from "react";
 import { rentContext } from "../App";
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 
-function Camera() {
+function R2E-800 RENTAL WEBSITE() {
   let data = [
     {
-      img: "https://m.media-amazon.com/images/I/914hFeTU2-L._SL1500_.jpg",
-      name: "Canon EOS 1500D 24.1 Digital SLR Camera (Black) with EF S18-55 is II Lens",
-      price: "500",
-    },
-    {
-      img: "https://m.media-amazon.com/images/I/81cEKnH692L._SL1500_.jpg",
-      name: "Canon EOS 200D II 24.1MP Digital SLR Camera + EF-S 18-55mm f4",
-      price: "1000",
-    },
-    {
-      img: "https://www.reliancedigital.in/medias/Nikon-D3500-DSLR-Camera-491431009-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w1NDYwMnxpbWFnZS9qcGVnfGltYWdlcy9oODcvaGFmLzkwNzAzNTYwNzA0MzAuanBnfDk1ODVmYTQ0NzY3Yzg5NTAyNzhkNjllMDNmYTE1MWM5MzNlMTRlNDQxNTUyZDI2YWMxMWYzYTlmNjNjNDc3ZjQ",
-      name: "Nikon D3500 DSLR Camera with AF-P 18-55 mm + AF-P 70-300 mm VR Kit",
-      price: "650",
-    },
-    {
-      img: "https://mozelo.s3.ap-south-1.amazonaws.com/wp-content/uploads/2019/10/05194253/Nikon-D3400-2.jpg",
-      name: "Nikon D3400 DSLR Camera with Single Lens NIKKOR 18-55 mm 16 GB SD Card",
-      price: "500",
-    },
-    {
-      img: "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/canon_eos_5d_mark_iv_1561852319_1274706.jpg",
-      name: "Canon EOS 5D Mark IV DSLR Camera with 24-105mm f/4L II Lens",
-      price: "2000",
-    },
-    {
-      img: "https://rukminim2.flixcart.com/image/416/416/jfbfde80/camera/n/r/n/canon-eos-eos-3000d-dslr-original-imaf3t5h9yuyc5zu.jpeg?q=70",
-      name: "Canon EOS 3000D DSLR Camera 1 Camera Body, 18 - 55 mm Lens (Black)",
-      price: "400",
-    },
-    {
-      img: "https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/nikon_d5600_dslr_camera_with_1484050840_1308819.jpg",
-      name: "Nikon D5600 DSLR Camera with 18-55mm Lens",
-      price: "600",
-    },
-    {
-      img: "https://image.shopmoment.com/resizer?quality=75&width=1320&fit=cover&gravityX=0.5&gravityY=0.5&height=1320&image=https://image.shopmoment.com/general/Moment-Sony-ILCE7M3K_B-Alpha-a7-III-Mirrorless-Digital-Camera-with-FE-28-70mm-OSS-lens-01.jpg?mtime=20210128121544&focal=none",
-      name: "Sony Alpha a7 III Full Frame Mirrorless Camera Body",
-      price: "1800",
+      img: "https://cs1.socpanel.com/cs1/project_images/KuS0V0SI8euhIGOUiUgXgrNLokhKPAl80sWr9Ldw.jpg",
+      name: "R2E-800 RENTAL WEBSITE ",
+      price: "800",
     },
   ];
 
@@ -95,4 +60,4 @@ function Camera() {
   );
 }
 
-export default Camera;
+export default R2E-800 RENTAL WEBSITE;
