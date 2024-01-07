71526da427dc74efa19a9d345d1ced4ee2e13f3a
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-07 17:52:43 +0800

    Update Suceess.js

--- src/Components/Suceess.js
+++ src/Components/Suceess.js
@@ -1,21 +1,24 @@
-// Imported Required Files
-import React,{useContext}  from 'react';
-import {Link,useParams} from 'react-router-dom'
-import {rentContext} from '../App';
+// Success.js 
+import React from 'react'
+  
 function Success() { 
-    return ( 
-        <div className="App" > 
-            <div> 
-                <h2 style={{ color: 'green' }}>                  
-                </h2> 
-                <h2>                  
-                </h2> 
-                <iframe width="560"
-                        height="315"
-                        src= "https://share.paybiz.ph/items/CPlSv6hMDywmJqzhJDbq7I"
-                       title="RENTAL FEE" > 
-                </iframe> 
-            </div> 
-        </div> 
-)}   
+    return ( 
+        <div className="App" > 
+            <div> 
+                <h2 style={{ color: 'green' }}> 
+                    GeeksforGeeks 
+                </h2> 
+                <h2> 
+                    How to create an Iframe in React 
+                </h2> 
+                <iframe width="560"
+                        height="315"
+                        src= 
+"https://www.youtube.com/embed/tmg6d3T_T6Q"
+                        title="GeeksforGeeks" > 
+                </iframe> 
+            </div> 
+        </div> 
+)} 
+  
 export default Success;
