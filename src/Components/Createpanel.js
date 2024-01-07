9ca42708eb3bd105e3c9dc19d5241e74be7fb193
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-01-08 00:36:09 +0800

    Update and rename Help.js to Createpanel.js

---
+++ src/Components/Createpanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CreatePanel() {
+  return (
+    <a href="https://echozone.socpanel.com/ref235084">
+      <button>
+        Click me!
+      </button>
+    </a>
+  );
+}
+
+export default CreatePanel;
